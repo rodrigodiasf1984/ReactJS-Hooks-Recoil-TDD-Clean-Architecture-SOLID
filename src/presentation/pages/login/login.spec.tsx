fcf253c8b8c8cb6ee6bd2bd81c59d48cc43440b3
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-09-21 13:58:24 +0200

    test: ensure login starts with the required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Login from "./Login";
+import { HiOutlineCheckCircle, HiOutlineXCircle } from "react-icons/hi";
 
 describe("Login Component", () => {
   test("should start start with initial state", () => {
@@ -10,5 +11,15 @@ describe("Login Component", () => {
 
     const submitButton = getByTestId("submitButton") as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
+
+    const emailStatus = getByTestId("email-status");
+    expect(emailStatus.title).toBe("Campo obrigatório");
+    // expect(emailStatus.textContent).toBe(<HiOutlineXCircle color="red" />);
+    expect(emailStatus.textContent).toBe("🔴");
+
+    const passwordStatus = getByTestId("password-status");
+    expect(passwordStatus.title).toBe("Campo obrigatório");
+    // expect(passwordStatus.textContent).toBe(<HiOutlineXCircle color="red" />);
+    expect(passwordStatus.textContent).toBe("🔴");
   });
 });
