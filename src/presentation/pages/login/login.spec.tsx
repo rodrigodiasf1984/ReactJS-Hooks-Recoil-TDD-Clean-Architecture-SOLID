c3d138484b77274d98f8c94410031118d1467408
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-09-22 00:03:29 +0200

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -54,12 +54,21 @@ describe("Login Component", () => {
     expect(passwordStatus.textContent).toBe("🔴");
   });
 
-  test("should call Validation with correct value", () => {
+  test("should call Validation with correct email value", () => {
     const { sut, validationSpy } = makeSut();
     const emailInput = sut.getByTestId("email");
     fireEvent.input(emailInput, { target: { value: "any_email" } });
     expect(validationSpy.input).toEqual({
       email: "any_email",
+    });
+  });
+
+  test("should call Validation with correct password value", () => {
+    const { sut, validationSpy } = makeSut();
+    const passwordInput = sut.getByTestId("password");
+    fireEvent.input(passwordInput, { target: { value: "any_password" } });
+    expect(validationSpy.input).toEqual({
+      password: "any_password",
     });
   });
 });
