29686fbd819a0485058686704111368dd4592f2d
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-14 22:33:04 +0200

    test: ensure EmailValidation returns false if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { InvalidFiedlError } from "@/validation/errors";
 import { EmailValidation } from "./email-validation";
+import faker from "faker";
 
 describe("EmailValidation", () => {
   test("Should return error if the email is invalid", () => {
-    const sut = new EmailValidation("email");
-    const error = sut.validate("");
+    const sut = new EmailValidation(faker.random.word());
+    const error = sut.validate(faker.random.word());
     expect(error).toEqual(new InvalidFiedlError());
+  });
+
+  test("Should return false if the email is valid", () => {
+    const sut = new EmailValidation(faker.random.word());
+    const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
   });
 });
