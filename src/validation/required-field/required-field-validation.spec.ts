de1544fb92591e2e88c06e7f2872ba7f191662ad
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-14 17:17:30 +0200

    refactor:  move SUT creation to a factoty method

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -2,9 +2,12 @@ import { RequiredFiedlError } from "@/validation/errors";
 import { RequiredFieldValidation } from "./required-field-validation";
 import faker from "faker";
 
+const makeSut = (): RequiredFieldValidation =>
+  new RequiredFieldValidation(faker.database.column());
+
 describe("RequiredFiedlValidation", () => {
   test("Should return an error if the field is empty", () => {
-    const sut = new RequiredFieldValidation("email");
+    const sut = makeSut();
     const error = sut.validate("");
     expect(error).toEqual(new RequiredFiedlError());
   });
