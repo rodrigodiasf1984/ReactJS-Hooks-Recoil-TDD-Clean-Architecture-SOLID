dc5170baecc0503fb8cd887bb963b63b8a7810b8
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-14 11:49:54 +0200

    feat: ensure Login navigate to Signup correctly

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -10,6 +10,7 @@ import Context from "@/presentation/context/form/form-context";
 import Styles from "./login-styles.scss";
 import { Validation } from "@/presentation/protocols/validation";
 import { Authentication } from "@/domain/usecases";
+import { Link } from "react-router-dom";
 
 type LoginProps = {
   validation: Validation;
@@ -85,7 +86,9 @@ const Login = ({ validation, authentication }: LoginProps) => {
           >
             Entrar
           </button>
-          <div className={Styles.link}>Criar conta</div>
+          <Link data-testid="signup" to="/signup" className={Styles.link}>
+            Criar conta
+          </Link>
           <FormStatus />
         </form>
       </Context.Provider>
