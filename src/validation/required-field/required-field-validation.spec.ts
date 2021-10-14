db1e55eb698ac890b3251597340e581395a52b97
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-14 17:10:34 +0200

    test: ensure RequiredFieldValidation returns false if the field isn't empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFiedlError } from "@/validation/errors";
 import { RequiredFieldValidation } from "./required-field-validation";
+import faker from "faker";
 
 describe("RequiredFiedlValidation", () => {
   test("Should return an error if the field is empty", () => {
     const sut = new RequiredFieldValidation("email");
     const error = sut.validate("");
     expect(error).toEqual(new RequiredFiedlError());
+  });
+
+  test("Should return false if the field isn't empty", () => {
+    const sut = new RequiredFieldValidation("email");
+    const error = sut.validate(faker.random.word());
+    expect(error).toBeFalsy();
   });
 });
