3fc0936a5c509910ec979495a64c7a441519a775
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-15 23:05:38 +0200

    refactor: moved SUT creation to a factory method

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,16 +1,20 @@
 import { InvalidFiedlError } from "@/validation/errors";
 import { MinlengthValidation } from "./min-length-validation";
+import faker from "faker";
+
+const makeSut = (): MinlengthValidation =>
+  new MinlengthValidation(faker.database.column(), 5);
 
 describe("MinlengthValidation", () => {
   test("Should return error if value is less then 5 characters ", () => {
-    const sut = new MinlengthValidation("fieldName", 5);
-    const error = sut.validate("123");
+    const sut = makeSut();
+    const error = sut.validate(faker.random.alphaNumeric(4));
     expect(error).toEqual(new InvalidFiedlError());
   });
 
   test("Should return false if value is equal or greater then 5 characters", () => {
-    const sut = new MinlengthValidation("fieldName", 5);
-    const error = sut.validate("1234567");
+    const sut = makeSut();
+    const error = sut.validate(faker.random.alphaNumeric(5));
     expect(error).toBeFalsy();
   });
 });
