6ec5d992f7c8533bddd6b583d169ad722ced081d
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-15 22:58:04 +0200

    test: ensure MinLengthValidation returns false if value is equal or greater then 5

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -2,9 +2,15 @@ import { InvalidFiedlError } from "@/validation/errors";
 import { MinlengthValidation } from "./min-length-validation";
 
 describe("MinlengthValidation", () => {
-  test("Should return error if ", () => {
+  test("Should return error if value is less then 5 characters ", () => {
     const sut = new MinlengthValidation("fieldName", 5);
     const error = sut.validate("123");
     expect(error).toEqual(new InvalidFiedlError());
+  });
+
+  test("Should return false if value is equal or greater then 5 characters", () => {
+    const sut = new MinlengthValidation("fieldName", 5);
+    const error = sut.validate("1234567");
+    expect(error).toBeFalsy();
   });
 });
