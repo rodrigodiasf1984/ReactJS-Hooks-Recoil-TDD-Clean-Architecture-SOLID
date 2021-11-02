0a6591477499f2d8df2b9047b8d45cd5a848d67f
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-11-03 00:08:15 +0100

    chore: update jest config to ignore some files

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    "<rootDir>/src/**/*.{ts,tsx}",
-    "!<rootDir>/src/main/**/*",
-    "!<rootDir>/src/**/index.ts",
-    "!<rootDir>/src/presentation/components/router/**/*",
-    "!**/*.d.ts",
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/presentation/components/router/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts'
   ],
-  coverageDirectory: "coverage",
-  testEnvironment: "jsdom",
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
   transform: {
-    ".+\\.(ts|tsx)$": "ts-jest",
+    '.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
-    "@/(.*)": "<rootDir>/src/$1",
-    "\\.scss$": "identity-obj-proxy",
-  },
-};
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
+  }
+}
